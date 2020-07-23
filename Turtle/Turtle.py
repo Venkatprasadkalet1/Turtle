8b54485f2b6518bdf52c8494bd3970c933edf5fa
Venkat Prasad Kalet <venkatprasadkalet@gmail.com>
2020-07-23 11:57:23 +0530

    Add files via upload

---
+++ Turtle/Turtle.py
@@ -0,0 +1,26 @@
+import turtle
+
+turtle.speed(5)
+turtle.bgcolor("black")
+
+
+def curve():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(1)
+
+
+turtle.color("blue", "red")
+
+turtle.begin_fill()
+turtle.left(140)
+turtle.forward(111.65)
+curve()
+
+turtle.left(120)
+curve()
+turtle.forward(111.65)
+turtle.end_fill()
+turtle.hideturtle()
+turtle.mainloop()
+
