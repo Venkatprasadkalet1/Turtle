2ffd92f2fc134da8b92b1f6fab1c7b380178879d
suvanbalu <suvanbalu@gmail.com>
2021-10-03 20:23:30 +0530

    fixed errors

---
+++ Turtle/heart.py
@@ -0,0 +1,30 @@
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor("black")
+
+
+def curve():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(1)
+
+
+turtle.color("blue", "red")
+
+turtle.begin_fill()
+turtle.left(140)
+
+turtle.forward(112.65)
+turtle.forward(111.64)
+
+curve()
+
+turtle.left(120)
+curve()
+turtle.forward(111.64)
+turtle.end_fill()
+turtle.hideturtle()
+turtle.mainloop()
+turtle.mainloop()
+#ankit
